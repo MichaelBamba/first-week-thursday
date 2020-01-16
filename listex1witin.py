6b7545b514d1a9f7e4d6cd5a93c1787161c8f566
MichaelBamba <mjbamba2@gmail.com>
2020-01-16 16:59:18 -0500

    adding with lists

---
+++ listex1witin.py
@@ -0,0 +1,5 @@
+numbers_to_add = [ int(x) for x in input("put numbers here").split()]
+
+answer = sum(numbers_to_add)
+
+print (answer)
---
+++ listexer1.py
@@ -0,0 +1,5 @@
+numberstoadd = [2, 3, 4, 5, 6]
+
+answer = sum(numberstoadd)
+
+print(answer)
