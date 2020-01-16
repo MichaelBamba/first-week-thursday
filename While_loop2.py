88fb1552e65f76ed0b72b0d76df66acc002bba3c
MichaelBamba <mjbamba2@gmail.com>
2020-01-16 10:24:21 -0500

    first upload of today

---
+++ While_loop2.py
@@ -0,0 +1,7 @@
+title = "Pokemon Masters"
+
+count = 0
+
+while count < len(title):
+    print (count)
+    count = count + 1
---
+++ while_loop3.py
@@ -0,0 +1,7 @@
+title = "Pokemon Masters"
+
+count = 0
+
+while count < len(title):
+    print (title[count])
+    count = count + 1
